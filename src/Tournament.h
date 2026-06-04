0e5276e44a5c3b7068afecc313e8cbd3ac753a06
Gabriel Silveira <silveira.b.gabriel@gmail.com>
2026-06-04 18:07:11 -0300

    Feat getTournamentResuts

--- src/Tournament.h
+++ src/Tournament.h
@@ -1,10 +1,12 @@
 #include "Pokemon.h"
+#include "graph.h"
+#include "battle.h"
 
 class Tournament{
     private:
         std::vector<Pokemon*> team1;
         std::vector<Pokemon*> team2;
-        std::
+        std::unordered_map<std::string, Graph> battles;
 
 
     public:
@@ -13,8 +15,14 @@ class Tournament{
             this.team2 = team2;
         }
 
-        
-
-
-    
+        std::unordered_map<std::string, Graph> getTournamentResults(){
+            for(Pokemon pokemonTeam1 : team1){
+                for(Pokemon pokemonTeam2 : team2){
+                    //generateBattle
+                    Graph battle = new Graph(); 
+                    battles.insert({pokemonTeam1->name + " vs " + pokemonTeam2->name, battle.generate()});
+                }
+            }
+            return battles;
+        }
 }
