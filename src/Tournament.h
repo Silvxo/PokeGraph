39f3f3a76778a518d116d636fa081290b84a844b
Gabriel Silveira <silveira.b.gabriel@gmail.com>
2026-06-04 17:31:55 -0300

    Simple tournament structure

---
+++ src/Tournament.h
@@ -0,0 +1,20 @@
+#include "Pokemon.h"
+
+class Tournament{
+    private:
+        std::vector<Pokemon*> team1;
+        std::vector<Pokemon*> team2;
+        std::
+
+
+    public:
+        Tournament(std::vector<Pokemon*> team1, std::vector<Pokemon*> team2){
+            this.team1 = team1;
+            this.team2 = team2;
+        }
+
+        
+
+
+    
+}
